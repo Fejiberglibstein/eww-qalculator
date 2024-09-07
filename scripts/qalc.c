a1ddd048f89e48bff816a62bdb6ec0c53c592b38
Austin Thein <amthein321@gmail.com>
2024-09-07 01:29:31 -0400

    Replace double quotes with single quotes in parser

--- scripts/qalc.c
+++ scripts/qalc.c
@@ -189,11 +189,18 @@ Token parse_token(char *tok) {
     }
 
     char *val = NULL;
-    if (strlen(&(tok[i])) != 0) {
-        val = malloc(strlen(&(tok[i])));
+    int len = strlen(&(tok[i]));
+    if (len != 0) {
+        val = calloc(len + 1, sizeof(char));
         strcpy(val, &(tok[i]));
     }
 
+    // Replace all double quotes with single quotes
+    for (int i = 0; i < len; i++) {
+        if (val[i] == '"') {
+            val[i] = '\'';
+        }
+    }
     return (Token){.value = val, .ansi = seq};
 }
 
