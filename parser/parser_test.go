d66c7d80478e0b2c6529d08c67a1e8e2648fb0cd
Austin Thein <amthein321@gmail.com>
2024-11-17 23:40:18 -0500

    More test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -24,4 +24,25 @@ func TestAnsiSeq(t *testing.T) {
 	if seq != expected {
 		t.Error("seq not expected, got", seq)
 	}
+
+	inp = "[10mfoo"
+
+	seq, offset, err = parseAnsiSeq(inp)
+	if err != nil {
+		t.Error("Got error ", err)
+	}
+
+	if offset != 4 {
+		t.Error("expected offset 6, got ", offset)
+	}
+
+	expected = ansiSeq{
+		graphics: 10,
+		color:    0,
+	}
+
+	if seq != expected {
+		t.Error("seq not expected, got", seq)
+	}
+
 }
