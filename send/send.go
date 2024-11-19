2cd632fefbf412c8f59b54b584f925cb7ecb9e15
Austin Thein <amthein321@gmail.com>
2024-11-18 21:30:33 -0500

    Panic not Fatal

--- send/send.go
+++ send/send.go
@@ -13,32 +13,32 @@ import (
 
 func Send(args []string) {
 	if len(args) < 2 {
-		log.Fatal("Not enough args for send request")
+		log.Panic("Not enough args for send request")
 	}
 
 	conn, err := net.Dial("unix", server.Port)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 	defer conn.Close()
 
 	// Get the message to send based on args
 	message, err := getMessage(args)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
 	// Allocate a new byte buffer to fill with the bytes of our message
 	var buf bytes.Buffer
 	enc := gob.NewEncoder(&buf)
 	if err = enc.Encode(message); err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
 	// Write the buffer to the stream
 	_, err = conn.Write(buf.Bytes())
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
 }
