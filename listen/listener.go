2ca44308d1e063f0f573499e3a29c2ad7abd02dc
Austin Thein <amthein321@gmail.com>
2024-11-19 21:04:29 -0500

    Listener

--- listen/listener.go
+++ listen/listener.go
@@ -1,1 +1,57 @@
 package listen
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/Fejiberglibstein/eww-qalculator/message"
+	"github.com/Fejiberglibstein/eww-qalculator/server"
+)
+
+type Channel string
+
+const (
+	ExprChan   Channel = "expr"
+	ResultChan Channel = "result"
+)
+
+type listener struct {
+	channel Channel
+	conn    net.Conn
+}
+
+func Listen(channel Channel) {
+	conn, err := net.Dial("unix", server.Port)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer conn.Close()
+
+	l := listener{
+		channel: channel,
+		conn:    conn,
+	}
+
+	l.sendListenReq(channel)
+
+	for {
+		msg, err := message.ReadMessage(&conn)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+
+		fmt.Print(msg)
+	}
+}
+
+func (l *listener) sendListenReq(channel Channel) {
+	message.SendMessage(
+		&l.conn,
+		message.Message{
+			Header: uint8(message.Listen),
+			Data:   string(channel),
+		},
+	)
+}
