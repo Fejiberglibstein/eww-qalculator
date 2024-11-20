04ee69c688eef3ebce596bfe6e7e391e84f36674
Austin Thein <amthein321@gmail.com>
2024-11-19 20:49:49 -0500

    SendMessage function

--- message/message.go
+++ message/message.go
@@ -1,5 +1,12 @@
 package message
 
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+	"net"
+)
+
 const Port = "/tmp/eww-calc"
 
 type Request uint8
@@ -9,11 +16,20 @@ type Message struct {
 	Data   string
 }
 
-type ListenerPort string
+func SendMessage(conn *net.Conn, message Message) {
+	// Allocate a new byte buffer to fill with the bytes of our message
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	if err := enc.Encode(message); err != nil {
+		log.Panic(err)
+	}
 
-const (
-	ExprResultPort ListenerPort = "expr_result"
-)
+	// Write the buffer to the stream
+	_, err := (*conn).Write(buf.Bytes())
+	if err != nil {
+		log.Panic(err)
+	}
+}
 
 const (
 	Listen Request = iota
